40f977feaa8a76d720dfd37f2a10fa54c970c1de
Chikire <chikireakuibe@gmail.com>
2026-03-17 12:50:06 -0700

    add app and data path

--- tests/test_app_playwright.py
+++ tests/test_app_playwright.py
@@ -7,3 +7,16 @@ from playwright.sync_api import Page, expect
 from shiny.playwright import controller
 from shiny.pytest import create_app_fixture
 from shiny.run import ShinyAppProc
+
+# Example run command:
+# python -m pytest tests/test_app_playwright.py -v --browser firefox
+
+APP_PATH = Path(__file__).resolve().parents[1] / "src" / "app.py"
+app = create_app_fixture(str(APP_PATH))
+
+DATA_PATH = (
+    Path(__file__).resolve().parents[1]
+    / "data"
+    / "processed"
+    / "chocolate_sales_clean.csv"
+)
